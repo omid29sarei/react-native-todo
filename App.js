f7027e5f0d81cb98684eba806fdb736638acaf15
Omid Sarei <omidsarei29@gmail.com>
2021-05-16 12:18:02 +0200

    finishing the todo app

--- App.js
+++ App.js
@@ -1,17 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { KeyboardAvoidingView, StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { KeyboardAvoidingView, StyleSheet, Text, View, TextInput, TouchableOpacity, Keyboard } from 'react-native';
 import Task from './components/Task';
 
 export default function App() {
+  const [task, setTask] = React.useState();
+  const [taskItems, setTaskItems] = React.useState([]);
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task]);
+    setTask(null)
+  }
+  const handleDelete = (index) => {
+    const itemsCopy = [...taskItems];
+    itemsCopy.splice(index,1);
+    setTaskItems(itemsCopy);
+  }
   return (
     <View style={styles.container}>
       <View style={styles.tasksWrapper}>
         <Text style={styles.sectionTitle}>Todays's Task</Text>
         <View style={styles.itemsWrapper}>
-          <Task text={'This is a test'} />
-          <Task text={'This is a test'} />
-          <Task text={'This is a test'} />
+          {taskItems?.map((item, index) => {
+            return (
+              <TouchableOpacity key={index} onPress={() => handleDelete(index)}>
+                <Task text={item} />
+              </TouchableOpacity>
+            )
+          })}
         </View>
       </View>
 
@@ -19,8 +34,8 @@ export default function App() {
         behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
         style={styles.writeTaskWrapper}
       >
-        <TextInput style={styles.input} placeholder="Write a Task" />
-        <TouchableOpacity>
+        <TextInput style={styles.input} placeholder="Write a Task" value={task} onChangeText={text => setTask(text)} />
+        <TouchableOpacity onPress={() => handleAddTask()}>
           <View style={styles.addWrapper}>
             <Text style={styles.addText}>+</Text>
           </View>
@@ -66,12 +81,12 @@ const styles = StyleSheet.create({
   addWrapper: {
     width: 60,
     height: 60,
-    backgroundColor:'#FFF',
+    backgroundColor: '#FFF',
     borderRadius: 60,
-    alignItems:'center',
-    justifyContent:'center',
-    borderColor:'#C0C0C0',
-    borderWidth:1
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1
 
   },
   addText: {},
